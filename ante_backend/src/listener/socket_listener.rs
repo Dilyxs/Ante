047a9d20901b7704e9194b09999f47d7f114eca9
Adsayan Selvankumaran <selvan@adsayan.com>
2026-04-09 19:16:23 -0400

    now implemented websocket method, can now read through messages

--- ante_backend/src/listener/socket_listener.rs
+++ ante_backend/src/listener/socket_listener.rs
@@ -16,13 +16,21 @@ use tokio::sync::watch::{
     Receiver as WatchReceiver, Sender as WatchSender, channel as watch_channel,
 };
 //TODO: make a dyn type
-#[derive(Debug, Deserialize, Serialize)]
-pub struct ResponseToWebSocket {
-    response: String,
+pub enum ResponseToWebSocketCommandType {
+    RecentWinner,
+    RecentVote,
+    RecentPost,
+    RecentAnswer,
+}
+pub trait ResponseToWebSocket: Send {
+    fn get_response_type(&self) -> ResponseToWebSocketCommandType;
+    fn serialize(&self) -> Bytes;
 }
 pub struct WebsocketMessageCommnand {
-    message_type: WebSocketManagerCommandType,
-    user_channel: Option<Sender<ResponseToWebSocket>>,
+    message_type: Option<WebSocketManagerCommandType>,
+    user_channel: Option<Sender<Box<dyn ResponseToWebSocket>>>,
+    user_id: Option<i32>,
+    block_chain_event: Option<BlockchainEvent>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -33,9 +41,70 @@ pub enum WebSocketManagerCommandType {
     ConnectFeed,
     ConnectBountyID(i32),
 }
+#[derive(Debug, Deserialize, Serialize)]
+pub enum BlockchainEvent {
+    NewWinner,
+    NewVote,
+    NewPost,
+    NewAnswer,
+}
 pub struct WebSocketManager {
-    feed: HashMap<i32, Sender<ResponseToWebSocket>>,
-    bounty_room: HashMap<i32, Sender<ResponseToWebSocket>>,
+    feed: HashMap<i32, Sender<Box<dyn ResponseToWebSocket>>>,
+    bounty_room: HashMap<i32, Sender<Box<dyn ResponseToWebSocket>>>,
+}
+impl WebSocketManager {
+    pub fn init() -> Self {
+        WebSocketManager {
+            feed: HashMap::new(),
+            bounty_room: HashMap::new(),
+        }
+    }
+    pub async fn handle_websocket_messages(
+        &mut self,
+        mut reading_chan: Receiver<WebsocketMessageCommnand>,
+        cancel_chan: WatchReceiver<bool>,
+    )
+    //TODO: add a tokio::select with cancel_chan
+    {
+        while let Some(command) = reading_chan.recv().await {
+            if command.user_id.is_some() && command.message_type.is_some() {
+                let user_id = command.user_id.unwrap();
+                match command.message_type.unwrap() {
+                    WebSocketManagerCommandType::QuitWebsocket => {
+                        self.feed.remove(&user_id);
+                        self.bounty_room.remove(&user_id);
+                    }
+                    WebSocketManagerCommandType::QuitBountyID(bounty_id) => {
+                        self.bounty_room.remove(&bounty_id);
+                    }
+                    WebSocketManagerCommandType::QuitFeed => {
+                        self.feed.remove(&user_id);
+                    }
+                    WebSocketManagerCommandType::ConnectFeed => {
+                        if let Some(user_channel) = command.user_channel {
+                            self.feed.insert(user_id, user_channel);
+                        }
+                    }
+                    WebSocketManagerCommandType::ConnectBountyID(bounty_id) => {
+                        if let Some(user_channel) = command.user_channel {
+                            self.bounty_room.insert(bounty_id, user_channel);
+                        }
+                    }
+                }
+                continue;
+            }
+            if command.block_chain_event.is_some() {
+                let event = command.block_chain_event.unwrap();
+                //TODO: eventually build out custom message for each type after parsing is done!
+                match event {
+                    BlockchainEvent::NewWinner => for (_, channel) in self.feed.iter() {},
+                    BlockchainEvent::NewVote => for (_, channel) in self.feed.iter() {},
+                    BlockchainEvent::NewPost => for (_, channel) in self.feed.iter() {},
+                    BlockchainEvent::NewAnswer => {}
+                }
+            }
+        }
+    }
 }
 pub struct IDManager {
     id: i32,
@@ -70,8 +139,8 @@ pub async fn handle_websocket(
     };
     let (mut sender, mut receiver) = socket.split();
     let (mut user_sender_Chan, mut user_reading): (
-        Sender<ResponseToWebSocket>,
-        Receiver<ResponseToWebSocket>,
+        Sender<Box<dyn ResponseToWebSocket>>,
+        Receiver<Box<dyn ResponseToWebSocket>>,
     ) = tokio::sync::mpsc::channel(10);
     let cancel_chan_clone = cancel_chan.clone();
     //this is for reading chan
@@ -86,8 +155,10 @@ pub async fn handle_websocket(
                     if let Ok(valid_content) = possible_content {
                         inner_websocket_manager_chan
                             .send(WebsocketMessageCommnand {
-                                message_type: valid_content.change_spot,
+                                message_type: Some(valid_content.change_spot),
                                 user_channel: None,
+                                user_id: Some(user_id),
+                                block_chain_event: None,
                             })
                             .await;
                     }
@@ -95,8 +166,10 @@ pub async fn handle_websocket(
             } else {
                 inner_websocket_manager_chan
                     .send(WebsocketMessageCommnand {
-                        message_type: WebSocketManagerCommandType::QuitWebsocket,
+                        message_type: Some(WebSocketManagerCommandType::QuitWebsocket),
                         user_channel: None,
+                        user_id: Some(user_id),
+                        block_chain_event: None,
                     })
                     .await;
             }
@@ -105,19 +178,18 @@ pub async fn handle_websocket(
     //now send to the websocket_manager_chan the chan to which it can then write to
     websocket_manager_chan
         .send(WebsocketMessageCommnand {
-            message_type: WebSocketManagerCommandType::ConnectFeed,
+            message_type: Some(WebSocketManagerCommandType::ConnectFeed),
             user_channel: Some(user_sender_Chan),
+            user_id: Some(user_id),
+            block_chain_event: None,
         })
         .await;
     //write channel
     tokio::spawn(async move {
         //:TODO: add a tokio::select with cancel_chan
         while let Some(valid_message) = user_reading.recv().await {
-            if let Ok(valid_json) = serde_json::to_vec(&valid_message) {
-                sender
-                    .send(Message::Binary(Bytes::copy_from_slice(&valid_json)))
-                    .await;
-            }
+            let valid_json = valid_message.serialize();
+            sender.send(Message::Binary(valid_json)).await;
         }
     });
 }
